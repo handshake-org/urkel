421debdbd2f3c864a6676010a4ea1e30b08984f1
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-08-01 22:17:11 -0700

    pkg: add shortcuts.

---
+++ radix.js
@@ -0,0 +1,13 @@
+/*!
+ * urkel.js - merkelized tree
+ * Copyright (c) 2018, Christopher Jeffrey (MIT License).
+ * https://github.com/handshake-org/urkel
+ */
+
+'use strict';
+
+/*
+ * Expose
+ */
+
+module.exports = require('./lib/radix/urkel');
