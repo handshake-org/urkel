b8fc01106df19fe2aaa948e20f20bf386e163a31
Chase Smith <chasesmith@Chases-MacBook-Pro.local>
2019-06-24 21:08:07 -0700

    More unit tests

---
+++ test/file-test.js
@@ -0,0 +1,36 @@
+/* eslint-env mocha */
+/* eslint prefer-arrow-callback: "off" */
+/* eslint no-unused-vars: "off" */
+
+/*
+Tests for the MFS
+*/
+
+'use strict';
+
+const assert = require('bsert');
+const File = require('../lib/file.js');
+
+/*
+Test chdir for file path not string, should throw error
+*/
+async function Test() {
+  assert(true, true, "Always should be right")
+}
+
+
+
+
+
+
+
+  describe("File", function() {
+    this.timeout(5000);
+
+    /*
+    it('should ...', async () => {
+      await Test();
+    });
+    */
+
+  });
